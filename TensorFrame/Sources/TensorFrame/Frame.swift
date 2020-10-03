0555c22d2aaa1d5c519395b1e23573392d011946
Rafael H Tibães <r@fael.nl>
2020-10-03 12:12:44 -0300

    Series & Frame

--- TensorFrame/Sources/TensorFrame/Frame.swift
+++ TensorFrame/Sources/TensorFrame/Frame.swift
@@ -1,39 +1,28 @@
 import TensorFlow
 import Accelerate
 
-public struct TensorFrame {
-    private(set) var data: Any
+
+public struct Frame {
+    private(set) var data: Array<Series> = []
     private(set) var rows: Int = 0
     private(set) var columns: Int = 0
-    private(set) var columns_names: Dictionary<AnyHashable, Int>
-    private(set) var columns_types: [Any]
+    private(set) var columns_names: Dictionary<String, Int> = [:]
     
     public var size: Int { columns * rows }
     
-    
     // MARK - Initialization
     
-    public init<T: TensorFlowScalar>(_ array: [T], column name: String = "0") {
-        self.data = [Tensor<T>(array)]
-        self.rows = array.count
-        self.columns = 1
-        self.columns_names = [name: 0]
-        self.columns_types = [Float.self]
-    }
-    
-    // empty
-    // matrix: [[T]]
-    // array: Tensor / [Tensor]
-    // dict
-    // zeros, ones, repeated, random
-    
-    public init(copy: TensorFrame) {
+    public init(copy: Frame) {
         self.data = copy.data
         self.rows = copy.rows
         self.columns = copy.columns
         self.columns_names = copy.columns_names
-        self.columns_types = copy.columns_types
     }
+    
+    // matrix: [[T]]
+    // array: Tensor / [Tensor]
+    // dict
+    // zeros, ones, repeated, random
     
     // MARK - Indexing / Slicing
     
@@ -43,31 +32,49 @@ public struct TensorFrame {
     private func isValid(column: Int) -> Bool {
         column >= 0 && column < columns
     }
-//    private func isValid(column: String) -> Bool {}
+    
+    private func isValid(column: String) -> Bool {
+        self.columns_names.keys.contains(column)
+    }
+    
     private func isValid(rows: ClosedRange<Int>) -> Bool {
         isValid(row: rows.lowerBound) && isValid(row: rows.upperBound)
     }
     private func isValid(columns: ClosedRange<Int>) -> Bool {
         isValid(column: columns.lowerBound) && isValid(column: columns.upperBound)
     }
-//    private func isValid(columns: [String]) -> Bool
+    
+    private func isValid(columns: [String]) -> Bool {
+        columns.allSatisfy{ isValid(column: $0) }
+    }
     
     public subscript(row: Int, column: Int) -> Any {
         get{
             precondition(isValid(row: row) && isValid(column: column),
-                         "Index out of range")
+                         "Frame: Index out of range")
             return data[column][row]
         }
         set {
             precondition(isValid(row: row) && isValid(column: column),
-                         "Index out of range")
-            precondition(type(of: newValue) == columns_types[column],
-                         "Types mismatch")
+                         "Frame: Index out of range")
             self.data[column][row] = newValue
         }
     }
     
-//    public subscript(row: Int, column: String) -> Any {}
+    public subscript(row: Int, column: String) -> Any {
+        get{
+            precondition(isValid(row: row) && isValid(column: column),
+                         "Frame: Index out of range")
+            let c: Int = columns_names[column]!
+            return data[c][row]
+        }
+        set {
+            precondition(isValid(row: row) && isValid(column: column),
+                         "Frame: Index out of range")
+            let c: Int = columns_names[column]!
+            self.data[c][row] = newValue
+        }
+    }
     
     // public tensorfy(from columns: [String], casting: Float) {}
     // transpose
